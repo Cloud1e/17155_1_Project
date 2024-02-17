07b7f37fb4a536f2bf09951b5bc087509df03536
Yizhou Wang <yizhouwang@utexas.edu>
2024-02-16 23:22:47 -0600

    http methods

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
-@app.route('/login')
-def login():
-    return 'Login Page'
-
 @app.route('/admin')
 def hello_admin():
     return 'Hello Admin'
@@ -18,12 +14,21 @@ def hello_user():
     name = request.args.get('name')
     return 'Hello %s' % name
 
-@app.route('/login_success/<name>')
-def hello(name):
+@app.route('/success/<name>')
+def success(name):
     if name == 'admin':
         return redirect(url_for('hello_admin'))
     else:
         return redirect(url_for('hello_user', name = name))
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name = user))
 
 @app.route('/projects/<project>')
 def project_detail(project):
