62f072009d392ed4b91fb5b1ca62df82fd460654
Cloud1e <ChenboYin1@gmail.com>
2024-02-26 23:45:54 -0600

    Add UserManagement.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import LoginForm from "./LoginForm"; 
-
+import UserManagementForm from "./UserManagement";
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <UserManagementForm />
     </div>
   );
 }
