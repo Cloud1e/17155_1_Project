53c203989717b23a1977c0969de1d03b5a8a17b4
Tianyu Shi <shity112@gmail.com>
2024-04-17 22:44:51 -0500

    update test_user

--- test/test_app.py
+++ test/test_app.py
@@ -4,4 +4,14 @@ def test_index():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
+    
+def test_admin():
+    tester = app.test_client()
+    response = tester.get('/admin')
+    assert response.status_code == 200
+    
+def test_admin():
+    tester = app.test_client()
+    response = tester.get('/home/')
+    assert response.status_code == 200
 
--- test/test_user.py
+++ test/test_user.py
@@ -1,14 +1,26 @@
-# from app import app
-# import unittest
+from app import app
+import unittest
 
-# class UserTest(unittest.TestCase):
-#     def setUp(self):
-#         self.client = app.test_client()
-#         self.client.testing = True
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
 
-#     def test_createUser(self):
-#         username = "user2"
-#         password = "password"
-#         response = self.client.post('/createUser', json={'username': username, 'password': password})
-#         self.assertEqual(response.status_code, 201)
-#         self.assertIn("User " + username + " Created!", response.json['message'])
+    def test_createUser(self):
+        username = "user4"
+        password = "password"
+        response = self.client.post('/createUser/', json={'username': username, 'password': password})
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        username = "user1"
+        password = "password"
+        response = self.client.post('/login/', json={'username': username, 'password': password})
+        self.assertEqual(response.status_code, 200)
+        
+    def test_logout(self):
+        response = self.client.post('/logout/')
+        self.assertEqual(response.data.decode('utf-8'), '1')
+        
+    
+
